d99efeb71319a97573fdd81f614023b6c69cb5a1
mirpatel <mirpatel@ebay.com>
2018-08-02 10:23:42 -0700

    Delete folders.

--- java/array/RotatingArray.java
+++
@@ -1,6 +0,0 @@
-public class RotatingArray {
-  
-  public static void main(String[] args) {
-  
-  }
-}
--- java/string/StringSolutions.java
+++
@@ -1,6 +0,0 @@
-public class StringSolutions {
-  
-  public static void main(String[] args) {
-  
-  }
-}
