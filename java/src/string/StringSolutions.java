6d9738dd2fa823852e8397213c9eb5a48ac77d57
mirpatel <mirpatel@ebay.com>
2018-08-02 22:32:08 -0700

    Isomprphic strings

--- java/src/string/StringSolutions.java
+++ java/src/string/StringSolutions.java
@@ -1,5 +1,7 @@
 package string;
 
+import java.util.*;
+
 public class StringSolutions {
 
     public static void main(String[] args) {
@@ -7,6 +9,7 @@ public class StringSolutions {
         // reverse words in a string
         // https://www.programcreek.com/2014/05/leetcode-reverse-words-in-a-string-ii-java/
         System.out.println("reverseWords(\"the sky is blue\")" + reverseWords("the sky is blue"));
+        System.out.println("are isomorphic : " + areIsomorphic("ab","aa"));
     }
 
     /**
@@ -34,5 +37,38 @@ public class StringSolutions {
         }
 
         return output.toString();
+    }
+
+    private static boolean areIsomorphic(String s, String t) {
+        if (null == s || null == t) {
+            return false;
+        }
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        final int SIZE = 256;
+        boolean[] visited = new boolean[SIZE]; // to check visited characters in t
+        int[] mapping = new int[SIZE];
+        Arrays.fill(mapping, -1);
+
+        for (int i = 0; i < s.length(); i++) {
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+
+            if (mapping[sChar] == -1) {
+                if (visited[tChar] == true) {
+                    return false;
+                }
+
+                mapping[sChar] = tChar;
+                visited[tChar] = true;
+            } else {
+                if (mapping[sChar] != tChar) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
