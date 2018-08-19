13acf752089e0f0665c1ebd5bdaa3b280ff596cb
mirpatel <mirpatel@ebay.com>
2018-08-18 17:52:04 -0700

    - find intersecting node

--- java/src/linkedlist/LinkedListSolution.java
+++ java/src/linkedlist/LinkedListSolution.java
@@ -3,6 +3,8 @@ package linkedlist;
 import linkedlist.helper.ListNodeHelper;
 import linkedlist.pojo.ListNode;
 
+import java.util.Stack;
+
 public class LinkedListSolution {
 
     public static void main(String[] args) {
@@ -32,6 +34,12 @@ public class LinkedListSolution {
         ListNode n2 = ListNodeHelper.generateLinkedList(new int[]{2,5,9,10});
         System.out.println("merge 2 sorted : " + mergeSorted(n1, n2));
 
+        // merge k sorted linked lists
+        n1 = ListNodeHelper.generateLinkedList(new int[]{1,4,7});
+        n2 = ListNodeHelper.generateLinkedList(new int[]{2,5,9,10});
+        ListNode n3 = ListNodeHelper.generateLinkedList(new int[]{3,8,11,12});
+        System.out.println("merge K sorted : " + mergeKSorted(new ListNode[]{n1, n2, n3}));
+
         // merge 2 sorted linked lists in place
         n1 = ListNodeHelper.generateLinkedList(new int[]{1,4,7});
         n2 = ListNodeHelper.generateLinkedList(new int[]{2,5,9,10});
@@ -41,8 +49,24 @@ public class LinkedListSolution {
         n1 = ListNodeHelper.generateLinkedList(new int[]{9,9});
         n2 = ListNodeHelper.generateLinkedList(new int[]{1});
         System.out.println("add 2 numbers : " + addTwoNumbers(n1, n2));
+
+        // remove nth from last in the list
+        node = ListNodeHelper.generateLinkedList(new int[]{1,2,3,4,5});
+        System.out.println("remove nth from last = " + removeNthFromEnd(node, 3));
+
+        // intersection node
+        n1 = ListNodeHelper.generateLinkedList(new int[]{1});
+        n2 = ListNodeHelper.generateLinkedList(new int[]{1});
+        System.out.println("intersection node : " + getIntersectionNode(n1, n2));
+
     }
 
+    /**
+     * Delete node with given value from list
+     *
+     * @param head
+     * @param val
+     */
     private static void deleteFromList(ListNode head, int val) {
         if (null == head) {
             return;
@@ -91,6 +115,12 @@ public class LinkedListSolution {
         return head;
     }
 
+    /**
+     * Reverse list recursively
+     *
+     * @param list
+     * @return node {@link ListNode}
+     */
     private static ListNode reverseRecursive(ListNode list) {
         if (list == null) return null; // first question
 
@@ -113,6 +143,12 @@ public class LinkedListSolution {
         return reverseRest;
     }
 
+    /**
+     * Delete node with given value from list
+     *
+     * @param head
+     * @param n
+     */
     private static void delete(ListNode head, int n) {
         if (null == head) {
             return;
@@ -155,7 +191,7 @@ public class LinkedListSolution {
             return n1;
         }
 
-        // initilize head of the result
+        // initialize head of the result
         ListNode result = null;
         if (n1.val < n2.val) {
             result = new ListNode(n1.val);
@@ -166,6 +202,7 @@ public class LinkedListSolution {
         }
         ListNode head = result;
 
+        // merge 2 lists upto equal length of the min length list
         while (null != n1 && null != n2) {
             if (n1.val < n2.val) {
                 result.next = new ListNode(n1.val);
@@ -177,12 +214,14 @@ public class LinkedListSolution {
             result = result.next;
         }
 
+        // merge n1 if there are elements still remaining
         while (null != n1) {
             result.next = new ListNode(n1.val);
             n1 = n1.next;
             result = result.next;
         }
 
+        // merge n2 if there are elements still remaining
         while (null != n2) {
             result.next = new ListNode(n2.val);
             n2 = n2.next;
@@ -245,6 +284,35 @@ public class LinkedListSolution {
         return result;
     }
 
+    /**
+     * Merge given k sorted linked lists
+     *
+     * @param lists
+     * @return
+     */
+    private static ListNode mergeKSorted(ListNode[] lists) {
+        if (null == lists || lists.length == 0) {
+            return null;
+        }
+        if (lists.length < 2) {
+            return lists[0];
+        }
+
+        ListNode result = lists[0];
+        for (int i = 1; i < lists.length; i++) {
+            result = mergeSorted(result, lists[i]);
+        }
+
+        return result;
+    }
+
+    /**
+     * Add given two numbers
+     *
+     * @param l1
+     * @param l2
+     * @return sum {@link ListNode}
+     */
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         if (null == l1 && null == l2) {
             return null;
@@ -312,5 +380,122 @@ public class LinkedListSolution {
         // reverse the list before returning
         //return reverse(head);
         return head;
+    }
+
+    /**
+     * Check if given linked list has a cycle
+     *
+     * @param head
+     * @return
+     */
+    public boolean hasCycle(ListNode head) {
+        if (null == head || head.next == null) {
+            return false;
+        }
+
+        ListNode fast = head.next;
+        ListNode slow = head;
+
+        while (null != fast && null != fast.next && fast.val != slow.val) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        if (null != fast && null != fast.next && fast.val == slow.val) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Remove nth node from last in the list
+     *
+     * @param head
+     * @param n
+     * @return ListNode
+     */
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        if (null == head || n < 0) {
+            return null;
+        }
+
+        ListNode fast = head;
+        int i = 0;
+        // increment fast to point to 'nth' node from start
+        while (null != fast && i != n) {
+            fast = fast.next;
+            i++;
+        }
+
+        if (fast == null) {
+            if (n == 1) {
+                return null;
+            } else if (n != i) { // n is greater than the length of the linked list
+                return head;
+            }
+        }
+
+        ListNode prev = null;
+        ListNode current = head;
+        ListNode result = head;
+
+        while (null != fast) {
+            fast = fast.next;
+            prev = current;
+            current = current.next;
+        }
+
+        // delete the node (set prev.next = current.next)
+        if (null == prev) { // remove head
+            ListNode temp = head.next;
+            result = temp;
+        } else {
+            prev.next = current.next;
+        }
+
+        return result;
+    }
+
+    /**
+     * Get the intersecting node in the 2 lists
+     * @param headA
+     * @param headB
+     *
+     * @return intersecting {@link ListNode}
+     */
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (null == headA || null == headB) {
+            return null;
+        }
+
+        Stack<ListNode> stackA = new Stack();
+        Stack<ListNode> stackB = new Stack();
+
+        // push elements from A and B to respective stacks
+        while (null != headA) {
+            stackA.push(headA);
+            headA = headA.next;
+        }
+        while (null != headB) {
+            stackB.push(headB);
+            headB = headB.next;
+        }
+
+        // pop stacks at the same time and compare values
+        // the moment we come across non-matching value, the node after that is the
+        // intersection node
+        ListNode prev = null;
+        while (!stackA.isEmpty() && !stackB.isEmpty()) {
+            ListNode first = stackA.pop();
+            ListNode second = stackB.pop();
+            if (first.val == second.val) {
+                prev = first;
+                continue;
+            } else {
+                break;
+            }
+        }
+        return prev; // last matched node
     }
 }
