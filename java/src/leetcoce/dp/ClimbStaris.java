301a601ef2a6886451cdeb9758ec6f461050a00e
Miral <miralkumar.patel@gmail.com>
2019-10-17 10:03:43 -0700

    - Add Pythagorian triplets - Add fibonacci based problems.

--- java/src/leetcoce/dp/ClimbStaris.java
+++ java/src/leetcoce/dp/ClimbStaris.java
@@ -11,13 +11,13 @@ public class ClimbStaris {
      * You are climbing a stair case. It takes n steps to reach to the top.
      * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
      * Note: Given n will be a positive integer.
-     *
+     * <p>
      * Input: 2
      * Output: 2
      * Explanation: There are two ways to climb to the top.
      * 1. 1 step + 1 step
      * 2. 2 steps
-     *
+     * <p>
      * It is fibonacci series
      *
      * @param n
@@ -28,14 +28,14 @@ public class ClimbStaris {
             return n;
         }
 
-        int[] dp = new int[n+1];
-        dp[1] = 1;
-        dp[2] = 2;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
 
-        for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
 
-        return dp[n];
+        return dp[n - 1];
     }
 }
--- java/src/programcreek/matrix/WordSearch.java
+++ java/src/programcreek/matrix/WordSearch.java
@@ -2,4 +2,53 @@ package programcreek.matrix;
 
 public class WordSearch {
 
+    public static void main(String[] args) {
+        System.out.println(hasWord("Miral", new char[][]{
+                {'a', 'b', 'c', 'd'},
+                {'i', 'm', 'n', 'o'},
+                {'j', 'i', 'r', 'a'},
+                {'d', 'e', 'f', 'l'}
+        }));
+    }
+
+    public static boolean hasWord(String word, char[][] grid) {
+        if (null == grid || grid.length == 0 ||
+            null == grid[0] || grid[0].length == 0 ||
+            null == word || word.isEmpty()) {
+            return false;
+        }
+
+        int m = grid.length, n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == word.charAt(0) && search(grid, word, i, j, 0)) {
+                        return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean search(char[][] grid, String word, int i, int j, int k) {
+        if (k == word.length()) {
+            return true;
+        }
+
+        if (i < 0 || i >= grid.length ||
+            j < 0 || j >= grid[0].length || grid[i][j] != word.charAt(k)) {
+            return false;
+        }
+
+        char temp = grid[i][j];
+        grid[i][j] = '#';
+        if (search(grid, word, i-1, j, k+1) ||
+            search(grid, word, i+1, j, k+1) ||
+            search(grid, word, i, j-1, k+1) ||
+            search(grid, word, i, j+1, k+1)) {
+            return true;
+        }
+        grid[i][j] = temp;
+        return false;
+    }
 }
